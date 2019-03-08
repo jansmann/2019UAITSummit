661f7bdf3a420c21506ea00d79befb031e0729c0
Christopher H. Jansmann <jansmann@email.arizona.edu>
2019-03-08 09:55:55 -0700

    Added placeholders for where code should go during the demo.

--- Examples/WriteOnce/WriteOnceMobile/WriteOnceMobile/App.xaml.cs
+++ Examples/WriteOnce/WriteOnceMobile/WriteOnceMobile/App.xaml.cs
@@ -7,10 +7,24 @@ namespace WriteOnceMobile
 {
     public partial class App : Application
     {
+        /// <summary>
+        /// This is the root page of our application.  When the program launches, items in this section will 
+        /// be run across all three platforms.
+        /// </summary>
+        /// <remarks>
+        /// Some third party components will require you to add material in this method in order to correctly
+        /// wire them up for use.
+        /// </remarks>
         public App()
         {
+            //  Base call.
             InitializeComponent();
 
+            //  Load and launch the main page of this application.  We've conveniently
+            //  called it "MainPage" but you can specify object of type ContentPage.
+            //  If necessary, this call may be overridden and a code-derived view could be passed in.
+
+            //  TODO: 02-Add a button in code here.
             MainPage = new MainPage();
         }
 
